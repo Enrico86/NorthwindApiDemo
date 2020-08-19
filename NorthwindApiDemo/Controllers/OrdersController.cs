4b2a0c0e7d45f8d1dc363500f6a6dbf84a0410bb
Enrico86 <64355652+Enrico86@users.noreply.github.com>
2020-08-19 08:52:05 +0200

    New post request for the creation of a new order

--- NorthwindApiDemo/Controllers/OrdersController.cs
+++ NorthwindApiDemo/Controllers/OrdersController.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NorthwindApiDemo.Models;
 
 namespace NorthwindApiDemo.Controllers
 {
     [Route("api/customers")]
     public class OrdersController : Controller
     {
-        [HttpGet("{customerId}/orders")]
+        [HttpGet("{customerId}/orders", Name ="GetOrders")]
         public IActionResult GetOrders (int customerId)
         {
             var customer = Repository.Instance.Customers.FirstOrDefault(s => s.ID == customerId);
@@ -21,7 +22,8 @@ namespace NorthwindApiDemo.Controllers
             return Ok(customer.Orders);
         }
 
-        [HttpGet("{customerId}/orders/{orderId}")]
+        [HttpGet("{customerId}/orders/{orderId}", Name ="GetOrder")]
+        //Asigno un nombre a esta ruta, para poderlo utilizar en otros métodos en que lo necesite
         public IActionResult GetOrder (int customerId, int orderId)
         {
             var customer = Repository.Instance.Customers.FirstOrDefault(s => s.ID == customerId);
@@ -35,6 +37,52 @@ namespace NorthwindApiDemo.Controllers
                 return NotFound($"Pedido {orderId} no encontrado para el cliente {customer.ContactName}");
             }
             return Ok(order);
+        }
+
+        [HttpPost("{customerId}/orders")]
+        public IActionResult CreateOrder (int customerId, [FromBody]OrdersForCreationDTO order)
+            //Hay que especificar que el parametro order será introducio por el usuario en el Body de la página web de 
+            //donde se está invocando este método (api/customers/3/orders por ejemplo si se trata del cliente con id 3)
+        {
+            if (order==null)
+            {
+                return BadRequest("Petición incorrecta: el pedido está incompleto");
+            }
+            var customer = Repository.Instance.Customers.FirstOrDefault(s => s.ID == customerId);
+            if (customer == null)
+            {
+                return NotFound($"Cliente con id {customerId} no encontrado");
+            }
+            var maxOrderId = Repository.Instance.Customers.SelectMany(c=>c.Orders).Max(o=>o.OrderId);
+            //Capturo en una variable el id max entre todos los pedidos de todos los clientes
+
+            var finalOrder = new OrdersDTO()
+            {
+                OrderId = maxOrderId++,
+                CustomerId = order.CustomerId,
+                EmployeeId=order.EmployeeId,
+                Freight=order.Freight,
+                OrderDate=DateTime.Today,
+                RequiredDate=order.RequiredDate,
+                ShipAddress=order.ShipAddress,
+                ShipCity=order.ShipCity,
+                ShipCountry=order.ShipCountry,
+                ShipName=order.ShipName,
+                ShippedaDate=order.ShippedaDate,
+                ShipPostalCode=order.ShipPostalCode,
+                ShipRegion=order.ShipRegion,
+                ShipVia=order.ShipVia
+                //El usuario digamos creará un OrdersForCretionDTO (por el que no tiene que definir el OrderId ni el OrderDate por ejemplo
+            };
+
+            customer.Orders.Add(finalOrder);
+            return CreatedAtRoute("GetOrder", new { customerId = customerId, orderId = finalOrder.OrderId },finalOrder);
+            //Como acción final vamos a crear una nueva ruta para que el navegador nos dirija hacia ella: necesita tres 
+            //parametros esta ruta, el primero es el nombre de la ruta (que hemos llemado GetOrder, hubiera sido lo mismo 
+            //volver a escribir "{customerId}/orders/{orderId}"), el segundo son los valores de la ruta ya que esta ruta 
+            //necesita de dos variables para que podamos navegar hacia ella (el customerId y orderId) por lo que creamos 
+            //una nueva instancia de un objeto anonimo con estos dos valores, cogiendolos del pedido que acabamos de crear.
+            //Y como tercer parametro le pasamos el objeto del que queremos que coja los datos a mostrar en el navegador.
         }
     }
 }
---
+++ NorthwindApiDemo/Models/OrdersForCreationDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindApiDemo.Models
+{
+    public class OrdersForCreationDTO
+    {
+        public string CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime RequiredDate { get; set; }
+        public DateTime ShippedaDate { get; set; }
+        public int ShipVia { get; set; }
+        public decimal Freight { get; set; }
+        public string ShipName { get; set; }
+        public string ShipAddress { get; set; }
+        public string ShipCity { get; set; }
+        public string ShipRegion { get; set; }
+        public string ShipPostalCode { get; set; }
+        public string ShipCountry { get; set; }
+    }
+}
